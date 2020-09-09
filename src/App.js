3045978fd002ed9128ad6216ade837694a317ab9
quang.nguyen <nguyenquang06012000@gmail.com>
2020-09-09 10:51:11 +0700

    Cart - redux

--- src/App.js
+++ src/App.js
@@ -6,80 +6,80 @@ import Products from "./components/Products";
 import Filter from "./components/filter";
 import Cart from "./components/Cart";
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      cartItems: JSON.parse(localStorage.getItem("cartItems")) || [],
-      products: data.products,
-      size: "",
-      sort: "",
-    };
-  }
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     cartItems: JSON.parse(localStorage.getItem("cartItems")) || [],
+  //     // products: data.products,
+  //     // size: "",
+  //     // sort: "",
+  //   };
+  // }
   createOderAll = (order) => {
     alert("Need To save  order for " + order.name);
   };
-  removeFromCart = (product) => {
-    const cartItems = this.state.cartItems.slice();
-    this.setState({
-      cartItems: cartItems.filter((x) => x._id !== product._id),
-    });
-    localStorage.setItem("cartItems", JSON.stringify(this.state.cartItems));
-  };
-  addToCart = (product) => {
-    const cartItems = this.state.cartItems.slice();
-    let alreadyInCart = false;
-    cartItems.forEach((item) => {
-      if (item._id === product._id) {
-        item.count++;
-        alreadyInCart = true;
-        // al
-      }
-    });
-    if (!alreadyInCart) {
-      cartItems.push({ ...product, count: 1 });
-    }
-    this.setState({
-      cartItems,
-    });
-    localStorage.setItem("cartItems", JSON.stringify(this.state.cartItems));
-  };
-  sortProducts = (e) => {
-    console.log(e.target.value);
-    const sort = e.target.value;
-    this.setState({
-      sort: e.target.value,
-      products: this.state.products
-        .slice()
-        .sort((a, b) =>
-          sort === "lowest"
-            ? a.price < b.price
-              ? 1
-              : -1
-            : sort === "highest"
-            ? a.price > b.price
-              ? 1
-              : -1
-            : a._id < b._id
-            ? 1
-            : -1
-        ),
-    });
-  };
-  filterProducts = (e) => {
-    console.log(e);
-    if (e.target.value === "") {
-      this.setState({
-        size: e.target.value,
-        products: data.products,
-      });
-    } else
-      this.setState({
-        size: e.target.value,
-        products: data.products.filter(
-          (product) => product.availableSizes.indexOf(e.target.value) > -1
-        ),
-      });
-  };
+  // removeFromCart = (product) => {
+  //   const cartItems = this.state.cartItems.slice();
+  //   this.setState({
+  //     cartItems: cartItems.filter((x) => x._id !== product._id),
+  //   });
+  //   localStorage.setItem("cartItems", JSON.stringify(this.state.cartItems));
+  // };
+  // addToCart = (product) => {
+  //   const cartItems = this.state.cartItems.slice();
+  //   let alreadyInCart = false;
+  //   cartItems.forEach((item) => {
+  //     if (item._id === product._id) {
+  //       item.count++;
+  //       alreadyInCart = true;
+  //       // al
+  //     }
+  //   });
+  //   if (!alreadyInCart) {
+  //     cartItems.push({ ...product, count: 1 });
+  //   }
+  //   this.setState({
+  //     cartItems,
+  //   });
+  //   localStorage.setItem("cartItems", JSON.stringify(this.state.cartItems));
+  // };
+  // sortProducts = (e) => {
+  //   console.log(e.target.value);
+  //   const sort = e.target.value;
+  //   this.setState({
+  //     sort: e.target.value,
+  //     products: this.state.products
+  //       .slice()
+  //       .sort((a, b) =>
+  //         sort === "lowest"
+  //           ? a.price < b.price
+  //             ? 1
+  //             : -1
+  //           : sort === "highest"
+  //           ? a.price > b.price
+  //             ? 1
+  //             : -1
+  //           : a._id < b._id
+  //           ? 1
+  //           : -1
+  //       ),
+  //   });
+  // };
+  // filterProducts = (e) => {
+  //   console.log(e);
+  //   if (e.target.value === "") {
+  //     this.setState({
+  //       size: e.target.value,
+  //       products: data.products,
+  //     });
+  //   } else
+  //     this.setState({
+  //       size: e.target.value,
+  //       products: data.products.filter(
+  //         (product) => product.availableSizes.indexOf(e.target.value) > -1
+  //       ),
+  //     });
+  // };
   render() {
     return (
       <div className="grid-container">
@@ -90,22 +90,22 @@ class App extends React.Component {
           <div className="content">
             <div className="main">
               <Filter
-                count={this.state.products?.length}
-                size={this.state.size}
-                sort={this.state.sort}
-                filterProducts={this.filterProducts}
-                sortProducts={this.sortProducts}
+              // count={this.state.products?.length}
+              // size={this.state.size}
+              // sort={this.state.sort}
+              // filterProducts={this.filterProducts}
+              // sortProducts={this.sortProducts}
               />
               <Products
-                products={this.state.products}
-                addToCart={this.addToCart}
+              // products={this.state.products}
+              // addToCart={this.addToCart}
               />
             </div>
             <div className="sidebar">
               <Cart
-                cartItems={this.state.cartItems}
-                removeFromCart={this.removeFromCart}
-                createOderAll={this.createOderAll}
+              // cartItems={this.state.cartItems}
+              // removeFromCart={this.removeFromCart}
+              // createOderAll={this.createOderAll}
               />
             </div>
           </div>
--- src/utils/redux/store.js
+++ src/utils/redux/store.js
@@ -1,11 +1,13 @@
 import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import thunk from "redux-thunk";
 import { productsReducer } from "./productReducer";
+import { cartReducer } from "./cartReducer";
 const initialState = {};
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   combineReducers({
     products: productsReducer,
+    cart: cartReducer,
   }),
   initialState,
   composeEnhancer(applyMiddleware(thunk))
